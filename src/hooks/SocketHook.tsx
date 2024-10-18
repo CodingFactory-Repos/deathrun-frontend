fd0d921adca289bcb06b025e86a21982283f329f
AyakorK <guillaume.moret@yahoo.com>
2024-10-18 16:21:29 +0200

    fix: remove props from socket call and add it to the creation of the room

--- src/hooks/SocketHook.tsx
+++ src/hooks/SocketHook.tsx
@@ -4,7 +4,6 @@ import { socket } from "../socket";
 function usePlayerPosition() {
   const [isConnected, setIsConnected] = useState(socket.connected);
   const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [props, setProps] = useState([{x: 0, y: 0}]);
 
   useEffect(() => {
     function onConnect() {
@@ -30,17 +29,12 @@ function usePlayerPosition() {
       setPosition({ x: Math.floor(data.x), y: Math.floor(data.y) });
     }
 
-    function onPropsReceived(data: [{ x: number, y: number }]) {
-      setProps(data);
-    }
-
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("players:move", onPositionChange);
-    socket.on("props:received", onPropsReceived);
   }, []);
 
-  return { isConnected, position, socket, props };
+  return { isConnected, position, socket };
 }
 
 export default usePlayerPosition;
--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,38 +1,46 @@
-import React, {useEffect} from "react";
-import {DndProvider, useDrag, useDrop} from "react-dnd";
-import {HTML5Backend} from "react-dnd-html5-backend";
-import MainPage from "../components/MainPage.tsx";
-import usePlayerPosition from "../hooks/SocketHook.tsx";
-import {useNavigate} from "react-router-dom";
-import TrapBlock from "../components/TrapBlock.tsx";
+import React, { useEffect, useState } from 'react';
+import { DndProvider, useDrop } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import MainPage from '../components/MainPage.tsx';
+import usePlayerPosition from '../hooks/SocketHook.tsx';
+import { useNavigate } from 'react-router-dom';
+import TrapBlock from '../components/TrapBlock.tsx';
+
+interface roomInformations {
+    code: string;
+    creator: string;
+    players: string[];
+    gods: string[];
+    props: { x: number; y: number }[];
+}
 
 const ItemTypes = {
-    ICON: "icon",
+    ICON: 'icon',
 };
 
 const iconsData = [
-    {id: 1, label: "⚡"},
-    {id: 2, label: "🔥"},
-    {id: 3, label: "🌟"},
+    { id: 1, label: '⚡' },
+    { id: 2, label: '🔥' },
+    { id: 3, label: '🌟' },
 ];
 
 const Cell = ({
-                  x,
-                  y,
-                  onDrop,
-                  hasPlayer,
-                  hasProps,
-              }: {
+    x,
+    y,
+    onDrop,
+    hasPlayer,
+    hasProps,
+}: {
     x: number;
     y: number;
     onDrop: (x: number, y: number, itemId: number) => void;
     hasPlayer: boolean;
     hasProps: boolean;
 }) => {
-    const [{isOver}, drop] = useDrop(() => ({
+    const [{ isOver }, drop] = useDrop(() => ({
         accept: ItemTypes.ICON,
         drop: (item: { id: number }) => onDrop(x, y, item.id),
-        collect: (monitor) => ({
+        collect: monitor => ({
             isOver: !!monitor.isOver(),
         }),
     }));
@@ -41,26 +49,25 @@ const Cell = ({
         <div
             ref={drop}
             style={{
-                border: "solid 1px black",
-                width: "2rem",
-                height: "2rem",
-                position: "relative",
-                backgroundColor: hasProps ? "gray" : isOver ? "lightgreen" : "white",
-                pointerEvents: hasProps ? "none" : "auto",
-            }}
-        >
+                border: 'solid 1px black',
+                width: '2rem',
+                height: '2rem',
+                position: 'relative',
+                backgroundColor: hasProps ? 'gray' : isOver ? 'lightgreen' : 'white',
+                pointerEvents: hasProps ? 'none' : 'auto',
+            }}>
             {/* If the player is in this case place a red dot */}
             {hasPlayer && (
                 <div
                     style={{
-                        width: "1rem",
-                        height: "1rem",
-                        backgroundColor: "red",
-                        borderRadius: "50%",
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        transform: "translate(-50%, -50%)",
+                        width: '1rem',
+                        height: '1rem',
+                        backgroundColor: 'red',
+                        borderRadius: '50%',
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
                     }}
                 />
             )}
@@ -69,19 +76,19 @@ const Cell = ({
 };
 
 const GameRows = ({
-                      rowIndex,
-                      playerPosition,
-                      onDrop,
-                      propsPlaced,
-                  }: {
+    rowIndex,
+    playerPosition,
+    onDrop,
+    propsPlaced,
+}: {
     rowIndex: number;
     playerPosition: { x: number; y: number };
     onDrop: (x: number, y: number, itemId: number) => void;
-    propsPlaced: [{x: number, y: number}];
+    propsPlaced: [{ x: number; y: number }];
 }) => {
     return (
         <>
-            {Array.from({length: 9}, (_, colIndex) => (
+            {Array.from({ length: 9 }, (_, colIndex) => (
                 <Cell
                     key={colIndex}
                     x={colIndex}
@@ -90,7 +97,9 @@ const GameRows = ({
                     hasPlayer={
                         colIndex === playerPosition.x && rowIndex === playerPosition.y
                     }
-                    hasProps={propsPlaced.some((prop) => prop.x === colIndex && prop.y === rowIndex)}
+                    hasProps={propsPlaced.some(
+                        prop => prop.x === colIndex && prop.y === rowIndex,
+                    )}
                 />
             ))}
         </>
@@ -98,51 +107,61 @@ const GameRows = ({
 };
 
 const Game: React.FC = () => {
-    const {isConnected, position, socket, props} = usePlayerPosition();
+    const { isConnected, position, socket } = usePlayerPosition();
     const navigate = useNavigate();
     const searchParams = new URLSearchParams(location.search);
-    const room = searchParams.get("player");
+    const room = searchParams.get('player');
+    const [props, setProps] = useState([{ x: 0, y: 0 }]);
 
-    console.log("Position", position);
-    console.log("isConnected", isConnected);
-    console.log("Props", props);
+    console.log('Position', position);
+    console.log('isConnected', isConnected);
+    console.log('Props', props);
 
-    let roomInformations: { code: string; creator: string; players: string[]; gods: string[] };
+    let roomInformations: roomInformations;
 
     useEffect(() => {
-        socket.on("rooms:join", (data: { code: string; creator: string; players: string[]; gods: string[] } | {
-            error: string
-        }) => {
-            if ("error" in data) {
-                alert(data.error);
-                navigate("/");
-            } else {
-                roomInformations = data;
-                console.log("Joined room", roomInformations);
-
-                // Après avoir rejoint la room plus besoin de l'écouter.
-                socket.on("rooms:events", (data: { code: string, creator: string, players: string[], gods: string[] }) => {
+        socket.on(
+            'rooms:join',
+            (
+                data:
+                    | roomInformations
+                    | {
+                        error: string;
+                    },
+            ) => {
+                if ('error' in data) {
+                    alert(data.error);
+                    navigate('/');
+                } else {
                     roomInformations = data;
-                    console.log("Room events", roomInformations);
-                });
-            }
+                    console.log('Joined room', roomInformations);
 
-            socket.off("rooms:join");
-        });
+                    setProps(roomInformations.props);
+
+                    // Après avoir rejoint la room plus besoin de l'écouter.
+                    socket.on('rooms:events', (data: roomInformations) => {
+                        roomInformations = data;
+                        console.log('Room events', roomInformations);
+                    });
+                }
+
+                socket.off('rooms:join');
+            },
+        );
 
         if (room) {
-            socket.emit("rooms:join", {code: room, joinAs: "god"});
+            socket.emit('rooms:join', { code: room, joinAs: 'god' });
         } else {
-            navigate("/");
+            navigate('/');
         }
     }, []);
 
     const handleDrop = (x: number, y: number, itemId: number) => {
         console.log(`Item ${itemId} dropped on cell (x: ${x}, y: ${y})`);
-        socket.emit("traps:request", {
+        socket.emit('traps:request', {
             x: x,
             y: y,
-            trapType: "crossbow_down_prefab",
+            trapType: 'crossbow_down_prefab',
         });
     };
 
@@ -151,31 +170,28 @@ const Game: React.FC = () => {
             <MainPage>
                 <div
                     style={{
-                        display: "flex",
-                        justifyContent: "center",
+                        display: 'flex',
+                        justifyContent: 'center',
                         marginBottom: 20,
-                    }}
-                >
+                    }}>
                     <h1>LoopTrap</h1>
                 </div>
                 <div
                     style={{
-                        display: "flex",
-                        justifyContent: "space-between",
-                    }}
-                >
+                        display: 'flex',
+                        justifyContent: 'space-between',
+                    }}>
                     <div
                         style={{
-                            display: "grid",
-                            gridTemplateColumns: "repeat(9, 2rem)",
-                            gridTemplateRows: "repeat(9, 2rem)",
-                        }}
-                    >
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(9, 2rem)',
+                            gridTemplateRows: 'repeat(9, 2rem)',
+                        }}>
                         {/* Using x and y */}
-                        {Array.from({length: 9})
+                        {Array.from({ length: 9 })
                             .map((_, rowIndex) => rowIndex)
                             .reverse()
-                            .map((rowIndex) => (
+                            .map(rowIndex => (
                                 <GameRows
                                     rowIndex={rowIndex}
                                     key={rowIndex}
@@ -185,7 +201,7 @@ const Game: React.FC = () => {
                                 />
                             ))}
                     </div>
-                    <TrapBlock trapItem={iconsData}/>
+                    <TrapBlock trapItem={iconsData} />
                 </div>
             </MainPage>
         </DndProvider>
