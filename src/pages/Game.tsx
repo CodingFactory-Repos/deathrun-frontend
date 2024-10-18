0cf3a64569f0534e1575b92f880cb85d84b617ca
BdBouba <156540511+BdBouba@users.noreply.github.com>
2024-10-18 17:12:40 +0200

    feat: Added Modal

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -1,11 +1,38 @@
 import React, {useEffect} from "react";
-import {DndProvider, useDrag, useDrop} from "react-dnd";
+import {DndProvider, useDrop} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
 import MainPage from "../components/MainPage.tsx";
 import usePlayerPosition from "../hooks/SocketHook.tsx";
 import {useNavigate} from "react-router-dom";
 import TrapBlock from "../components/TrapBlock.tsx";
 
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Backdrop from '@mui/material/Backdrop';
+import Modal from '@mui/material/Modal';
+import Fade from '@mui/material/Fade';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  
+};
 const ItemTypes = {
     ICON: "icon",
 };
@@ -139,6 +166,12 @@ const Game: React.FC = () => {
         });
     };
 
+
+      const [open, setOpen] = React.useState(false);
+      const handleOpen = () => setOpen(true);
+      const handleClose = () => setOpen(false);
+     
+
     return (
         <DndProvider backend={HTML5Backend}>
             <MainPage>
@@ -178,10 +211,49 @@ const Game: React.FC = () => {
                             ))}
                     </div>
                     <TrapBlock trapItem={iconsData}/>
+                </div>     
+                <Box
+                  component="form"
+                  sx={{ '& > :not(style)': { m: 1, width: '25ch' } }}
+                  noValidate
+                  autoComplete="off"
+                >               
+                 
+                </Box>        
+                <div>
+                  <Button onClick={handleOpen}>Ecrire au joeur</Button>
+                  <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    open={open}
+                    onClose={handleClose}
+                    closeAfterTransition
+                    slots={{ backdrop: Backdrop }}
+                    slotProps={{
+                      backdrop: {
+                        timeout: 500,
+                      },
+                    }}
+                  >
+                    <Fade in={open}>
+                      <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2" >
+                          Message à envoyer au joueur
+                        </Typography>
+                        <Stack id="transition-modal-description"sx={{gap: 2, width: "100%"}}>
+                          <TextField id="standard-basic" fullWidth label="Message" variant="standard" />
+                          <Stack spacing={2} direction="row">
+                            <Button variant="text">Envoyer </Button>
+                          </Stack>
+                        </Stack>
+                      </Box>
+                    </Fade>
+                  </Modal>
                 </div>
+
+            
             </MainPage>
         </DndProvider>
     );
 };
-
 export default Game;
