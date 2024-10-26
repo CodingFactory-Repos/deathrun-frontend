0f697a08572d88faa0f8638fc4c26cada7b9efa7
AyakorK <guillaume.moret@yahoo.com>
2024-10-26 20:20:06 +0200

    Revert "lint: Apply lint to SocketHook"
    This reverts commit 91ccbfda5a2ebee7293bab2d0e44a7eaa3fdd737.

--- src/hooks/SocketHook.tsx
+++ src/hooks/SocketHook.tsx
@@ -3,71 +3,71 @@ import { useEffect, useState } from "react";
 import { socket } from "../socket";
 
 function usePlayerPosition() {
-	const [isConnected, setIsConnected] = useState(socket.connected);
-	const [position, setPosition] = useState({ x: 0, y: 0 });
-	const [traps, setTraps] = useState([{ x: 0, y: 0 }]);
-	const [showPlayer, setShowPlayer] = useState(false);
-	const [cameraFrame, setCameraFrame] = useState("");
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [traps, setTraps] = useState([{ x: 0, y: 0 }]);
+  const [showPlayer, setShowPlayer] = useState(false);
+  const [cameraFrame, setCameraFrame] = useState("");
 
-	useEffect(() => {
-		function onConnect() {
-			console.log("Connected");
-			setIsConnected(true);
+  useEffect(() => {
+    function onConnect() {
+      console.log("Connected");
+      setIsConnected(true);
 
-			// socket.emit("traps:request", {
-			//   x: 1,
-			//   y: 4,
-			//   trapType: "crossbow_down_prefab",
-			// });
-			//
-			// console.log("traps:request");
-		}
+      // socket.emit("traps:request", {
+      //   x: 1,
+      //   y: 4,
+      //   trapType: "crossbow_down_prefab",
+      // });
+      //
+      // console.log("traps:request");
+    }
 
-		function onDisconnect() {
-			console.log("Disconnected");
-			setIsConnected(false);
-		}
+    function onDisconnect() {
+      console.log("Disconnected");
+      setIsConnected(false);
+    }
 
-		function onPositionChange(data: { x: number; y: number }) {
-			console.log("Position change", data);
-			setPosition({ x: Math.floor(data.x), y: Math.floor(data.y) });
-		}
+    function onPositionChange(data: { x: number; y: number }) {
+      console.log("Position change", data);
+      setPosition({ x: Math.floor(data.x), y: Math.floor(data.y) });
+    }
 
-		function onTrapsList(data: { x: number; y: number }[]) {
-			console.log("Traps list", data);
-			setTraps(data);
-		}
+    function onTrapsList(data: { x: number; y: number }[]) {
+      console.log("Traps list", data);
+      setTraps(data);
+    }
 
-		function onDisableTracking() {
-			console.log(
-				"Rooms corridor-----------------------------------------------------------------------------"
-			);
-			setShowPlayer(false);
-		}
+    function onDisableTracking() {
+      console.log(
+        "Rooms corridor-----------------------------------------------------------------------------"
+      );
+      setShowPlayer(false);
+    }
 
-		function onEnableTracking() {
-			console.log(
-				"Enable tracking-----------------------------------------------------------------------------"
-			);
-			setShowPlayer(true);
-		}
+    function onEnableTracking() {
+      console.log(
+        "Enable tracking-----------------------------------------------------------------------------"
+      );
+      setShowPlayer(true);
+    }
 
-		function onCameraFrame(data: string) {
-			setCameraFrame(data);
-		}
+    function onCameraFrame(data: string) {
+      setCameraFrame(data);
+    }
 
-		socket.on("connect", onConnect);
-		socket.on("disconnect", onDisconnect);
-		socket.on("players:move", onPositionChange);
-		socket.on("traps:list", onTrapsList);
-		socket.on("disable:tracking", onDisableTracking);
-		socket.on("enable:tracking", onEnableTracking);
-		socket.on("camera:sending", (data: string) => {
-			onCameraFrame(data);
-		});
-	}, []);
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("players:move", onPositionChange);
+    socket.on("traps:list", onTrapsList);
+    socket.on("disable:tracking", onDisableTracking);
+    socket.on("enable:tracking", onEnableTracking);
+    socket.on("camera:sending", (data: string) => {
+      onCameraFrame(data);
+    });
+  }, []);
 
-	return { isConnected, position, socket, traps, showPlayer, cameraFrame };
+  return { isConnected, position, socket, traps, showPlayer, cameraFrame };
 }
 
 export default usePlayerPosition;
