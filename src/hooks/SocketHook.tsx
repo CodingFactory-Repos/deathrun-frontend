7344d9d50850c93147309816fb3762d4de69a261
Guillaume MORET <90462045+AyakorK@users.noreply.github.com>
2024-10-28 10:39:43 +0100

    feat: Link rps from front to game (#10)
    * feat: Link rps from front to game
    
    * feat: Addition of the "auto" launching from the RPS
    
    * style: Improve the design of the modal
    
    * style: Improve a little bit more the style

--- src/hooks/SocketHook.tsx
+++ src/hooks/SocketHook.tsx
@@ -8,6 +8,8 @@ function usePlayerPosition() {
     const [traps, setTraps] = useState([{ x: 0, y: 0 }]);
     const [showPlayer, setShowPlayer] = useState(false);
     const [cameraFrame, setCameraFrame] = useState("");
+    const [rpsResult, setRpsResult] = useState<{ result: string; move: string } | null>(null);
+    const [rpsStart, setRpsStart] = useState(false);
 
     useEffect(() => {
         function onConnect() {
@@ -38,6 +40,17 @@ function usePlayerPosition() {
             setTraps(data);
         }
 
+        function onRpsResult(data: { result: string; move: string }) {
+            console.log("RPS result", data);
+            setRpsResult(data);
+            setRpsStart(false);
+        }
+
+        function onRpsStart() {
+            console.log("RPS start");
+            setRpsStart(true);
+        }
+
         function onDisableTracking() {
             console.log(
                 "Rooms corridor-----------------------------------------------------------------------------"
@@ -55,11 +68,13 @@ function usePlayerPosition() {
         function onCameraFrame(data: string) {
             setCameraFrame(data);
         }
-        
+
         socket.on("connect", onConnect);
         socket.on("disconnect", onDisconnect);
         socket.on("players:move", onPositionChange);
         socket.on("traps:list", onTrapsList);
+        socket.on("rps:start", onRpsStart);
+        socket.on("rps:results", onRpsResult);
         socket.on("disable:tracking", onDisableTracking);
         socket.on("enable:tracking", onEnableTracking);
         socket.on("camera:sending", (data: string) => {
@@ -67,7 +82,7 @@ function usePlayerPosition() {
         });
     }, []);
 
-    return { isConnected, position, socket, traps, showPlayer, cameraFrame };
+    return { isConnected, position, socket, traps, rpsResult, rpsStart, showPlayer, cameraFrame };
 }
 
 export default usePlayerPosition;
