f7943fafa94973526864089fd3d78a7b5b071477
AyakorK <guillaume.moret@yahoo.com>
2024-10-28 02:55:59 +0100

    feat: Addition of the "auto" launching from the RPS

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -217,7 +217,7 @@ const GameRows = ({
 };
 
 const Game: React.FC = () => {
-    const { isConnected, position, socket, traps, showPlayer } =
+    const { isConnected, position, socket, traps, showPlayer, rpsStart } =
         usePlayerPosition();
     const location = useLocation();
     const navigate = useNavigate();
@@ -240,6 +240,12 @@ const Game: React.FC = () => {
             setTrapsList(traps);
         }
     }, [traps]);
+
+    useEffect(() => {
+        if (rpsStart) {
+            setOpenRps(true);
+        }
+    }, [rpsStart]);
 
     useEffect(() => {
         socket.on(
